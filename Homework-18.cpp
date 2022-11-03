7855dd2fdbd802073a1fbc505b8a514122cd86b8
Константин Жегин <zhegink@ya.ru>
2022-11-03 22:57:25 +0300

    Добавьте файлы проекта.

---
+++ Homework-18.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class User {
+    string _name;
+    string _login;
+    string _pass;
+};
+class Message {
+    string _text;
+    string _sender;
+    string _receiver;
+};
+
+int main()
+{
+    
+}
