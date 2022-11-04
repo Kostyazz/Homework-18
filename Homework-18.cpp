c421efeba5591657c7fbc875cec637732a001053
Константин Жегин <zhegink@ya.ru>
2022-11-04 11:35:20 +0300

    input from file

--- Homework-18.cpp
+++ Homework-18.cpp
@@ -16,6 +16,8 @@ class Message {
     string _sender;
     string _receiver;
 	string _text;
+public:
+	Message(string sender, string receiver, string text) : _sender(sender), _receiver(receiver), _text(text) {};
 };
 
 int main()
@@ -30,11 +32,14 @@ int main()
 		if (!loginFile.is_open()) {
 			throw runtime_error("ERROR: Login file not found");
 		}
-		while (loginFile) {
+		while (! loginFile.eof()) {
 			string login;
 			string password;
 			string name;
 			loginFile >> login >> password >> name;
+			if (login.empty()) {
+				break;
+			}
 			loginFile.ignore();
 			if (name.empty()) {
 				throw runtime_error("ERROR: bad credentials in the file");
@@ -46,4 +51,32 @@ int main()
 		cerr << ex.what() << endl;
 		exit(1);
 	}
+
+	try {
+		ifstream messageFile;
+		messageFile.open("Messages.txt");
+		if (!messageFile.is_open()) {
+			throw runtime_error("ERROR: Message file not found");
+		}
+		while (! messageFile.eof()) {
+			string sender;
+			string receiver;
+			string text;
+			messageFile >> sender >> receiver;
+			if (sender.empty()) {
+				break;
+			}
+			getline(messageFile, text);
+			if (text.empty()) {
+				throw runtime_error("ERROR: bad data in the file");
+			}
+			messages.emplace_back(Message(sender, receiver, text));
+		}
+	}
+	catch (runtime_error& ex) {
+		cerr << ex.what() << endl;
+		exit(1);
+	}
+
+
 }
